4f680842fc2eadc0d9c8d3ba8931f723767627e9
Ranka <ranka.j.94@gmail.com>
2021-02-22 21:14:02 +0100

    user model, auth controller, register request, factories

---
+++ database/migrations/2021_02_22_180606_create_images_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('imageURL');
+            $table->unsignedBigInteger('gallery_id');
+            $table->foreign('gallery_id')
+                ->references('id')
+                ->on('galleries')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropForeign('images_gallery_id_foreign');
+            $table->dropColumn('gallery_id');
+        });
+    }
+}
